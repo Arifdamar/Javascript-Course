44641b878176485ae65f858d6faf42aea97f7277
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-03-19 17:02:47 +0300

    random or in-order slide transition by time

--- SliderApp/script.js
+++ SliderApp/script.js
@@ -26,7 +26,14 @@ var models = [{
 ];
 
 var index = 0;
-showSlide(index);
+
+var interval;
+var settings = {
+    duration: '2000',
+    random : false
+};
+
+init(settings);
 
 document.querySelector('.fa-arrow-left').addEventListener('click', function () {
     index--;
@@ -45,6 +52,43 @@ document.querySelector('.fa-arrow-right').addEventListener('click', function () 
     console.log(index);
     showSlide(index);
 });
+
+document.querySelectorAll('.arrow').forEach(function(element) {
+    element.addEventListener('mouseenter', function(){
+        clearInterval(interval);
+    })
+});
+
+document.querySelectorAll('.arrow').forEach(function(element) {
+    element.addEventListener('mouseleave', function(){
+        init(settings);
+    })
+})
+
+function init(settings) {
+    var lastIndex;
+    interval = setInterval(function () {
+
+        if(settings.random){
+            lastIndex = index;
+            // random index
+            while (lastIndex == index) {
+                index = Math.floor(Math.random() * models.length);
+            }
+            console.log(index);
+        }
+        else{
+            // increment index
+            if (models.length == index + 1) {
+                index = -1;
+            }
+            index++;
+            console.log(index);
+        }
+        showSlide(index);
+
+    },settings.duration);
+}
 
 function showSlide(index) {
     document.querySelector('.card-title').textContent = models[index].name;
