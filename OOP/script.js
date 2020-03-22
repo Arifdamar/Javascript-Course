0f8190aa23ecc1b32f429cb70b867b944cfe3a2f
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-03-22 22:50:26 +0300

    prototype

--- OOP/script.js
+++ OOP/script.js
@@ -1,3 +1,5 @@
+/*
+**Constructor**
 // Object literals
 // let arif = {
 //     name: 'arif',
@@ -22,8 +24,8 @@ let Person = function (name, yearOfBirth, job) {
     this.job = job;
     this.calculateAge = function () {
         return 2020 - this.yearOfBirth;
-    }
-}
+    };
+};
 
 let arif = new Person('arif', 1999, 'student');
 let omer = new Person('şule', 1996, 'nurse');
@@ -39,3 +41,34 @@ console.log(omer.name);
 console.log(omer.yearOfBirth);
 console.log(omer.job);
 console.log(omer.calculateAge());
+*/
+
+//**Prototype**
+
+let Person = function(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+};
+
+Person.prototype.calculateAge = function() {
+    return 2020 - this.yearOfBirth;
+};
+
+Person.prototype.getName = function() {
+    return this.name;
+};
+
+Person.prototype.lastName = 'Damar'
+
+let arif = new Person('arif', 1999, 'student');
+let sule = new Person('sule', 1996, 'nurse');
+
+console.log(arif.getName());
+console.log(arif.calculateAge());
+console.log(arif.hasOwnProperty("lastName"));
+
+console.log("*************")
+
+console.log(sule.getName());
+console.log(sule.calculateAge());
