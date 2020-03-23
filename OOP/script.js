d8096b1bcab8721cf3703c04186ad26e8a2e2cbe
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-03-23 18:08:05 +0300

    Built-in Constructors

--- OOP/script.js
+++ OOP/script.js
@@ -144,7 +144,8 @@ console.log(sule);
 console.log(sule.calculateAge());
 */
 
-// Prototypal Inheritance
+/*
+// **Prototypal Inheritance**
 
 let Person = function (name, yearOfBirth, job) {
     this.name = name;
@@ -177,19 +178,79 @@ let sule = new Teacher('Şule', 1996, 'Math');
 console.log(sule);
 console.log(sule.calculateAge());
 console.log(sule.greeting());
+*/
 
 
+//**Built-in Constructors**
+// String
+
+var str1 = 'Arif';
+var str2 = new String('Arif');
+
+console.log(str1);
+console.log(typeof(str1));
+console.log('***********************');
+console.log(str2);
+console.log(typeof(str2));
+
+if (str1 === 'Arif') {
+    console.log('Yep');
+}
+else{
+    console.log('Nope');
+}
+
+if (str1 === str2) {
+    console.log('Yep');
+}
+else{
+    console.log('Nope');
+}
+
+if (str1 == str2) {
+    console.log('Yep');
+}
+else{
+    console.log('Nope');
+}
+
+String.prototype.repeat = function(n) {
+    return new Array(n+1).join(this);
+}
+
+console.log('Arif'.repeat(5));
 
 
+// Number
+var num1 = 10;
+var num2 = new Number(10);
 
+// Boolean
+var bool1 = true;
+var bool2 = new Boolean(true);
 
+// Object
+var obj1 = {
+    name: 'Arif'
+}
+var obj2 = new Object({
+    name: 'Arif'
+});
 
+// Array
+var arr1 = ['Arif','Ömer','Utku','Yusuf'];
+var arr2 = new Array('Arif','Ömer','Utku','Yusuf');
 
+Array.prototype.remove = Array.prototype.remove || function(member) {
+    var index = this.indexOf(member);
 
+    if (index > -1) {
+        this.splice(index, 1);
+    }
+    return this;
+}
 
-
-
-
+console.log(arr1.remove('Ömer'));
 
 
 
