07ddf462a12be0e28c5fa85eb7c424b107b34e40
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-03-25 14:43:44 +0300

    Form validation with error handling

--- Error Handling/script.js
+++ Error Handling/script.js
@@ -1,3 +1,4 @@
+/*
 // ReferenceError
 // TypeError
 // SyntaxError
@@ -29,3 +30,40 @@ catch (e) {
 finally{
     console.log('Finally block');
 }
+*/
+
+document.getElementById('myBtn').addEventListener('click', function(e){
+
+    var name = document.getElementById('name');
+    var age = document.getElementById('age');
+    var errors = document.getElementById('errors');
+
+    errors.innerHTML = '';
+
+    try{
+        if(name.value.length === 0) {
+            throw new Error('Name is required');
+        }
+        if(name.value.length > 20) {
+            throw new Error('Name is too long');
+        }
+        if(age.value.length === 0) {
+            throw new Error('Age is required');
+        }
+        if (isNaN(age.value)) {
+            throw new Error('Age is not valid');
+        }
+
+        console.log('Form is submitted');
+    }
+    catch(e){
+        errors.innerHTML = e.message;
+    }
+    finally{
+        name.value = '';
+        age.value = '';
+    }
+    
+
+    e.preventDefault();
+})
