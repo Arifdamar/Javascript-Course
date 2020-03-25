1096df06a916f9534a65329e5629136f14f02ecf
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-03-25 15:53:59 +0300

    Adding quiz prototypes

--- Exercise(QuizApp)/app.js
+++ Exercise(QuizApp)/app.js
@@ -9,6 +9,33 @@ Question.prototype.checkAnswer = function(answer) {
     return this.answer === answer;
 }
 
+// Quiz Constructor
+function Quiz(questions){
+    this.questions = questions;
+    this.score = 0;
+    this.questionIndex = 0;
+}
+
+// Quiz Prototype
+Quiz.prototype.getQuestion = function(){
+    return this.questions[this.questionIndex];
+}
+
+// Quiz isFinished
+Quiz.prototype.isFinished = function(){
+    return this.questions.length === this.questionIndex;
+}
+
+// Quiz Guess
+Quiz.prototype.guess = function(answer){
+    var question = this.getQuestion();
+
+    if(question.checkAnswer(answer)){
+        this.score++;
+    }
+    this.questionIndex++;
+}
+
 var q1 = new Question(
     "What's the best programming language?",
     ["C#","JavaScript","Python","Java","C++"],
@@ -24,8 +51,22 @@ var q3 = new Question(
     ["C#","JavaScript","Python","Java","C++"],
     "Python");
 
-console.log(q1.checkAnswer('C#'));
-console.log(q1.checkAnswer('JavaScript'));
+var questions = [q1, q2, q3];
 
-console.log(q3.checkAnswer('Java'));
-console.log(q3.checkAnswer('Python'));
+// Start Quiz
+var quiz = new Quiz(questions);
+
+console.log(quiz.isFinished());
+
+console.log(quiz.getQuestion());
+quiz.guess("JavaScript");
+
+console.log(quiz.getQuestion());
+quiz.guess("Java");
+
+console.log(quiz.getQuestion());
+quiz.guess("Python");
+
+console.log(quiz.score);
+
+console.log(quiz.isFinished());
