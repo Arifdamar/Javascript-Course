825125bbd78070974733d7b7070e82f44dbf719b
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-03-26 22:35:00 +0300

    Spread Operator

--- ModernJS ES6+  ES7/script.js
+++ ModernJS ES6+  ES7/script.js
@@ -94,7 +94,7 @@ let evenES6 = arr.filter(x => x%2== 0);
 console.log(evenES6);
 */
 
-
+/*
 // 'this' keyword
 
 // ES5
@@ -148,3 +148,41 @@ function GameES6(){
 
 let playerES6 = new GameES6();
 playerES6.addLive();
+*/
+
+
+// Spread Operator
+
+function getTotal(a,b,c) {
+    return a + b + c;
+}
+console.log(getTotal(1,2,3));
+
+let numbers = [10,20,30];
+
+// ES5
+console.log(getTotal.apply(null, numbers));
+
+// ES6
+console.log(getTotal(...numbers));
+
+
+let arr1 = ['two', 'three'];
+let arr2 = ['one','four', 'five'];
+let arr3 = ['one',...arr1,'four','five'];
+
+arr1.push(...arr2);
+arr1.unshift(...arr2);
+
+
+console.log(arr1);
+console.log(arr3);
+
+let h1 = document.querySelector('h1');
+let divs = document.querySelectorAll('div');
+
+let tags = [h1,...divs];
+
+tags.forEach(tag => {tag.style.color = 'red'});
+
+console.log(tags);
