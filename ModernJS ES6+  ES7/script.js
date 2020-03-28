1bcfb15ddb19e58fee86e4c9be611a57c4bb42d8
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-03-28 23:45:25 +0300

    Destructuring

--- ModernJS ES6+  ES7/script.js
+++ ModernJS ES6+  ES7/script.js
@@ -188,7 +188,7 @@ tags.forEach(tag => {tag.style.color = 'red'});
 console.log(tags);
 */
 
-
+/*
 // Rest Parameters
 
 // ES5
@@ -225,3 +225,90 @@ function isDriver(age,...years){
 }
 
 isDriver(17,2000,2005,1999,2006,1998,2004,2001,2002,2003,2007,2008);
+*/
+
+
+// Destructuring
+
+
+// Destructuring Assignment
+
+var a, b, rest;
+
+[a,b] = [10,20];
+console.log(a);
+console.log(b);
+
+console.log("*************");
+
+[a,b,...rest] = [10,20,30,40,50,60,70,80,90,100];
+console.log(a);
+console.log(b);
+console.log(rest);
+
+console.log("*************");
+
+({a,b} = {a:10,b:20});
+console.log(a);
+console.log(b);
+
+console.log("*************");
+
+({a,b,...rest} = {a:10,b:20,c:30,d:40,e:50,f:60});
+console.log(a);
+console.log(b);
+console.log(rest);
+
+
+// Array Destructuring
+
+const arrConfig = ['localhost', '8080', '900'];
+
+// // ES5
+// var server = arrConfig[0];
+// var port = arrConfig[1];
+// var timeout = arrConfig[2];
+
+//console.log(server, port, timeout);
+
+// // ES6
+// const [server, port, timeout] = arrConfig;
+// console.log(server, port, timeout);
+
+
+// Object Destructuring
+
+const objConfig = {
+    server: 'localhost',
+    port: '8080',
+    timeout: '900'
+}
+
+// var server = objConfig.server;
+// var port = objConfig.port;
+// var timeout = objConfig.timeout;
+
+// const {server, port, timeout} = objConfig;
+
+// console.log(server, port, timeout);
+
+// let {server: s, port: p, timeout: t} = objConfig;
+// console.log(s, p, t);
+
+
+// const objConfig1 = {
+//     server1: 'localhost',
+//     por1t: '8080'
+// }
+
+// let {server1, port1, timeout1 = '800'} = objConfig1;
+// console.log(server1, port1, timeout1);
+
+// let {server, port, timeout = '800'} = objConfig;
+// console.log(server, port, timeout);
+
+
+const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+const [,,wed,,fri] = days;
+console.log(wed,fri);
