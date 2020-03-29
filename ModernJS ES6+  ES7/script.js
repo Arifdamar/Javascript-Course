2c633e4fd45ce7fd559193cac59df3a6ea906eea
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-03-29 23:53:57 +0300

    Sets

--- ModernJS ES6+  ES7/script.js
+++ ModernJS ES6+  ES7/script.js
@@ -391,7 +391,7 @@ for(let i of value){
 }
 */
 
-
+/*
 // Maps : key/value pairs (collection)
 
 let val;
@@ -458,3 +458,81 @@ var second = new Map([
 var merged = new Map([...first, ...second]);
 
 console.log(merged);
+*/
+
+
+// Sets (Collection - Unique value)
+
+let val;
+var mySet = new Set();
+
+mySet.add(1);
+mySet.add(2);
+mySet.add(2);
+mySet.add('two');
+mySet.add({a:1,b:2});
+mySet.add(4);
+
+var obj = {a:1,b:2};
+
+mySet.add(obj);
+
+val = mySet.has(1);
+val = mySet.has(3);
+val = mySet.has(obj);
+
+val = mySet.size;
+
+mySet.delete(1);
+
+console.log(val);
+console.log(mySet);
+
+console.log('***********************');
+
+for(let item of mySet){
+    console.log(item);
+}
+
+console.log('***********************');
+
+for(let item of mySet.keys()){
+    console.log(item);
+}
+
+console.log('***********************');
+
+for(let item of mySet.values()){
+    console.log(item);
+}
+
+console.log('***********************');
+
+for(let [key,value] of mySet.entries()){
+    console.log(key + ': ' + value);
+}
+
+console.log('***********************');
+
+console.log(Array.from(mySet));
+
+let mySet2 = new Set([1,2,3,4]);
+
+console.log(mySet2);
+
+console.log('***********************');
+// Intersect
+
+// var intersect = new Set(Array.from(mySet).filter(x => mySet2.has(x)));
+
+// console.log(intersect);
+
+var intersect = new Set([...mySet].filter(x => mySet2.has(x)));
+
+console.log(intersect);
+
+// Difference
+
+var difference = new Set([...mySet].filter(x => !mySet2.has(x)));
+
+console.log(difference);
