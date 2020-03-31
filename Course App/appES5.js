c64fc7c627e78d883862978369a5cb185aba1545
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-03-31 18:18:27 +0300

    Create UI constructor and essential methods

--- Course App/appES5.js
+++ Course App/appES5.js
@@ -1,7 +1,34 @@
+// Course constructor
 function Course(title, instructor, image) {
     this.title = title;
     this.instructor = instructor;
     this.image = image;
+}
+
+// UI constructor
+function UI() {
+
+}
+
+UI.prototype.addCourseToList = function(course){
+    const list = document.getElementById('course-list');
+
+    var html = `
+        <tr>
+            <td><img src="img/${course.image}"/></td>
+            <td>${course.title}</td>
+            <td>${course.instructor}</td>
+            <td><a href="#" class="btn btn-danger btn-sm">Delete</a></td>
+        </tr>
+    `;
+
+    list.innerHTML += html;
+}
+
+UI.prototype.clearControls = function() {
+    document.getElementById('title').value = "";
+    document.getElementById('instructor').value = "";
+    document.getElementById('image').value = "";
 }
 
 document.getElementById('new-course').addEventListener('submit',
@@ -14,6 +41,14 @@ function(e) {
     // create course object
     const course = new Course(title, instructor, image);
 
+    // Create UI
+    const ui = new UI();
+
+    // add course to list
+    ui.addCourseToList(course);
+
+    // clear controls
+    ui.clearControls();
 
     e.preventDefault();
 });
