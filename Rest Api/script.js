2fae646ce54b6bfddf40e56f80587611e1d481ea
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-04-02 21:57:04 +0300

    Rest Api HTTP GET exercise

---
+++ Rest Api/script.js
@@ -0,0 +1,69 @@
+document.querySelector('#getOne').addEventListener('click', getOne);
+document.querySelector('#getAll').addEventListener('click', getAll);
+
+
+function getOne() {
+    let id = document.getElementById('postid').value;
+    var url = "https://jsonplaceholder.typicode.com/posts/" + id;
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onload = function () {
+        if (this.status === 200) {
+            let post = JSON.parse(this.responseText);
+
+            let html =
+                `
+                <div class="card mb-3">
+                    <div class="card-header">
+                        ${post.id}-${post.title}
+                    </div>
+                    <div class="card-body">
+                        <blockquote class="blockquote mb-0">
+                            <p>
+                            ${post.body}
+                            </p>
+                        </blockquote>
+                    </div>
+                </div>
+                `;
+            document.querySelector('#results').innerHTML = html;
+        }
+    }
+
+    xhr.send();
+}
+
+function getAll() {
+    var url = "https://jsonplaceholder.typicode.com/posts";
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onload = function () {
+        if (this.status === 200) {
+            let posts = JSON.parse(this.responseText);
+
+            let html = "";
+
+            posts.forEach((post) => {
+                html +=
+                    `
+                <div class="card mb-3">
+                    <div class="card-header">
+                        ${post.title}
+                    </div>
+                    <div class="card-body">
+                        <blockquote class="blockquote mb-0">
+                            <p>
+                            ${post.body}
+                            </p>
+                        </blockquote>
+                    </div>
+                </div>
+                `;
+            })
+
+            document.querySelector('#results').innerHTML = html;
+        }
+    }
+
+    xhr.send();
+}
