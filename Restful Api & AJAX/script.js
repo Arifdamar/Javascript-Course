cc01d893564647981eb413b53fa8660b50d2bdda
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-04-02 18:52:04 +0300

    JSON exercise (load datas from .json file with a loading gif)

--- Restful Api & AJAX/script.js
+++ Restful Api & AJAX/script.js
@@ -1,3 +1,5 @@
+/*
+// AJAX and XHR object
 var xhr = new XMLHttpRequest();
 
 xhr.onreadystatechange = () => {
@@ -34,3 +36,43 @@ status:
 403: "Forbidden"
 404: "Not Found"
 */
+
+document.querySelector('#getEmployee').addEventListener('click', loadEmployee);
+
+function loadEmployee() {
+
+
+    let loadImage = document.querySelector('#loading');
+    loadImage.style.display = 'block';
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'employees.json', true);
+
+    setTimeout(() => {
+
+        xhr.onload = function () {
+            loadImage.style.display = 'none';
+            if (this.status === 200) {
+                let employees = JSON.parse(this.response);
+                console.log(employees);
+
+                let html = '';
+
+                employees.forEach((employee) => {
+                    html +=
+                        `<tr>
+                        <td>${employee.firstName}</td>
+                        <td>${employee.lastName}</td>
+                        <td>${employee.age}</td>
+                        <td>${employee.retired}</td>
+                    <tr>`;
+                });
+
+                document.querySelector('#employees').innerHTML = html;
+            }
+        }
+
+        xhr.send();
+    }, 1500);
+
+}
