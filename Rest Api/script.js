ac562f78f30c861c8ecdcdd2d44cb18df32e4a06
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-04-06 00:13:12 +0300

    Asynchronous programming with Promise

--- Rest Api/script.js
+++ Rest Api/script.js
@@ -93,7 +93,7 @@ function postData() {
 }
 */
 
-
+/*
 // **Asynchronous programming with callback functions**
 let products = [{
         id: 1,
@@ -145,3 +145,50 @@ addProduct({
         console.log(data);
     }
 });
+*/
+
+
+// **Asynchronous programming with Promise**
+let products = [{
+    id: 1,
+    name: 'Samsung Galaxy S9',
+    price: 8000
+},
+{
+    id: 2,
+    name: 'Samsung Galaxy S8',
+    price: 7000
+},
+{
+    id: 3,
+    name: 'Samsung Galaxy S7',
+    price: 6000
+}
+];
+
+function addProduct(prd, callback) {
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+            products.push(prd);
+            let added = false;
+            if (added) {
+                resolve();
+            } else {
+                reject('Error: 500');
+            }
+        },2000);
+    });
+}
+
+function getProducts() {
+    setTimeout(() => {
+        products.forEach(p => {
+            console.log(p.name);
+        })
+    }, 1000);
+}
+
+addProduct({id: 4,name: 'Samsung Galaxy S10',price: 9000})
+.then(getProducts)
+.catch(err => console.log(err));
