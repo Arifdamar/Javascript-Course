a786e30d4d875fc175dfe73d63e390381b91bd98
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-04-08 16:08:55 +0300

    Module exercise

--- Javascript Module/script.js
+++ Javascript Module/script.js
@@ -72,6 +72,7 @@ console.log(Module.increment());
 console.log(Module.decrement());
 */
 
+/*
 var Module = (function() {
     var privateMethod = function(){
 
@@ -85,3 +86,64 @@ var Module = (function() {
 })();
 
 Module.publicMethod();
+*/
+
+
+
+const products = [
+    {name: 'Samsung Galaxy S8', price:8000},
+    {name: 'Samsung Galaxy S8+', price:8500},
+    {name: 'Samsung Galaxy S9', price:9000},
+    {name: 'Samsung Galaxy S9+', price:9500}
+];
+
+var ProductController = (function(data){
+    // private members
+    var collections = data || [];
+
+    const addProduct = function(product){
+        collections.push(product);
+    }
+
+    const removeProduct = function(product){
+        var index = collections.indexOf(product);
+        if(index >= 0){
+            collections.splice(index, 1);
+        }
+    }
+
+    const getProducts = function(){
+        return collections;
+    }
+
+    // Public members
+    return {
+        addProduct,
+        removeProduct,
+        getProducts
+    }
+
+})(products);
+
+ProductController.addProduct(products[0]);
+ProductController.addProduct(products[1]);
+
+ProductController.removeProduct(products[0]);
+
+ProductController.addProduct(products[2]);
+ProductController.addProduct(products[3]);
+
+console.log(ProductController.getProducts());
+
+// Module Extending
+
+var extended = (function(m){
+    m.sayHello = function(){
+        console.log('Hello from extended module');
+    }
+
+    return m;
+})(ProductController || {});
+
+extended.sayHello();
+console.log(extended.getProducts());
