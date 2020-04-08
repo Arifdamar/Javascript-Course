55cf7e196240f423d86eba54c2e0aba8bfea7f57
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-04-08 23:13:26 +0300

    Creating data structures

--- OEM Calculator/app.js
+++ OEM Calculator/app.js
@@ -9,6 +9,32 @@ const StorageController = (function () {
 
 const ProductController = (function () {
 
+    // private
+    const Product = function (id, name, price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    const data = {
+        products: [
+            {id:0, name:'Monitor', price:100},
+            {id:1, name:'RAM', price:40},
+            {id:2, name:'Keyboard', price:90}
+        ],
+        selectedProduct: null,
+        totalPrice: 0
+    }
+
+    // public
+    return {
+        getProducts: function (){
+            return data.products;
+        },
+        getData: function (){
+            return data;
+        }
+    }
 
 })();
 
@@ -21,7 +47,17 @@ const UIController = (function () {
 
 // App Controller
 
-const AppController = (function (ProductCtrl, UICtrl) {
+const App = (function (ProductCtrl, UICtrl) {
 
+    return {
+        init: function (){
+            console.log('Starting app...');
+            const products = ProductCtrl.getProducts();
+
+
+        }
+    }
 
 })(ProductController, UIController);
+
+App.init();
