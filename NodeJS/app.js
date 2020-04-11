5c2a835fe2d0300977587f4da321958ac69f8b05
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-04-11 13:25:12 +0300

    Installing packages using npm

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -6,8 +6,17 @@ var b = 20;
 console.log(a+b);
 */
 
+/*
 // CommonJS Module Syntax
 const logger = require('./logger');
 
 logger.log('Hello');
 logger.log(logger.name);
+*/
+
+
+var _ = require('underscore');
+
+var numbers = [10, 5, 100, 2, 1000];
+
+console.log(_.min(numbers));
